f76098c7d5455cc52eaad1022b6222d2a3d3385e
LexFTW <lexmengual@gmail.com>
2020-04-24 13:34:23 +0200

    ca_ES

---
+++ config/resources/ca_ES.js
@@ -0,0 +1,11 @@
+const Resources = {
+  LOGIN_EMAIL: 'Correu Electrònic',
+  LOGIN_PASSWORD: 'Contrasenya',
+  LOGIN_SIGNIN: 'Iniciar Sessió',
+  LOGIN_SIGNIN_GOOGLE: 'Iniciar Sessió amb Google',
+  LOGIN_REGISTER: "Registra't",
+  READER_TITLE: 'Lector de glucosa',
+  READER_TEXT_BUTTON: 'Comença a llegir',
+}
+
+export default Resources;

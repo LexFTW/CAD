a34b7359dc2bfda603a61cea32694c3ba92d195f
LexFTW <lexmengual@gmail.com>
2020-04-25 16:28:38 +0200

    Sign Out Function

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -52,11 +52,9 @@ export default class LoginScreen extends Component {
       permissions: ['public_profile'],
     });
     if (type === 'success') {
-      // Get the user's name using Facebook's Graph API
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
       Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
     } else {
-      // type === 'cancel'
     }
   } catch ({ message }) {
     alert(`Facebook Login Error: ${message}`);
--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { Text, SafeAreaView  } from 'react-native';
+import { Text, SafeAreaView, Alert  } from 'react-native';
 import  base  from '../constants/styles/Styles';
+import { IconButton, Colors } from 'react-native-paper';
 import NavigationTop from './../components/NavigationTop';
 import Resources from './../config/resources/resources';
+import firebase from '../config/firebase';
 
 export default class UserScreen extends React.Component {
+  onSignOut(){
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        Alert.alert('You Sign Out!');
+      })
+      .catch(error => {
+        // An error happened.
+        console.log("error logging out" + error);
+      });
+  }
+
   render(){
     return (
       <SafeAreaView style={base.container}>
@@ -12,6 +27,12 @@ export default class UserScreen extends React.Component {
           title={Resources.PROFILE_HEADER}
         />
         <Text>{Resources.PROFILE_HEADER}</Text>
+        <IconButton
+          icon="logout"
+          color={Colors.white500}
+          size={20}
+          onPress={() => this.onSignOut()}
+        />
       </SafeAreaView>
     );
   }

5fa9fe4cd7f167da2546ab316e796a14e963982e
LexFTW <lexmengual@gmail.com>
2020-04-26 17:04:02 +0200

    update userscreen

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Text, SafeAreaView, Alert  } from 'react-native';
+import { Text, SafeAreaView, Alert, View  } from 'react-native';
 import  base  from '../constants/styles/Styles';
-import { IconButton, Colors } from 'react-native-paper';
+import { IconButton, Colors, Divider, Avatar } from 'react-native-paper';
 import NavigationTop from './../components/NavigationTop';
 import Resources from './../config/resources/resources';
 import firebase from '../config/firebase';
@@ -12,28 +12,57 @@ export default class UserScreen extends React.Component {
       .auth()
       .signOut()
       .then(() => {
-        Alert.alert('You Sign Out!');
         this.props.navigation.navigate('Login');
       })
       .catch(error => {
-        // An error happened.
-        console.log("error logging out" + error);
+        Alert.alert(error);
       });
   }
 
   render(){
     return (
-      <SafeAreaView style={base.container}>
-        <NavigationTop
-          title={Resources.PROFILE_HEADER}
-        />
-        <Text>{Resources.PROFILE_HEADER}</Text>
-        <IconButton
-          icon="logout"
-          color={Colors.white500}
-          size={20}
-          onPress={() => this.onSignOut()}
-        />
+      <SafeAreaView>
+        <View style={{backgroundColor: '#2069b2', alignSelf: 'stretch', textAlign: 'center', height: 250}}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 15, marginTop: 25,}}>
+            <IconButton
+            icon="settings"
+            color={Colors.white}
+            size={20}
+            />
+            <View style={{justifyContent:'center', alignItems: 'center'}}>
+              <Avatar.Text size={125} style={{borderWidth: 2, borderColor: 'white', padding: 5}} label="AM" labelStyle={{padding: 10, fontSize: 45}} />
+              <Text style={{color: 'white', fontSize: 22, marginTop: 10}}>Alexis Mengual</Text>
+              <Text style={{color: 'white', fontSize: 14, fontStyle: 'italic'}}>Diabetes Tipo 1</Text>
+            </View>
+            <IconButton
+            icon="logout"
+            color={Colors.white}
+            size={20}
+            onPress={() => this.onSignOut()}
+            />
+          </View>
+        </View>
+          <View style={{padding: 10}}>
+          <View style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15, flexDirection: 'row', alignSelf: 'stretch', justifyContent: 'space-between'}}>
+            <Text style={{paddingTop: 8, fontSize: 15}}>{Resources.PROFILE_SETTINGS_PROFILE}</Text>
+            <IconButton
+              icon="pencil"
+              color={'#333333'}
+              size={20}
+            />
+          </View>
+          <Divider />
+          <View style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15, flexDirection: 'row', alignSelf: 'stretch', justifyContent: 'space-between'}}>
+            <Text style={{paddingTop: 8, fontSize: 15}}>{Resources.PROFILE_SETTINGS_MEDICATION}</Text>
+            <IconButton
+              icon="pencil"
+              color={'#333333'}
+              size={20}
+              animated={true}
+            />
+          </View>
+          <Divider />
+        </View>
       </SafeAreaView>
     );
   }

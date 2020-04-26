dd4015efdb5bf901b392f22198d8a320bc61e60d
David <xldaviduson@gmail.com>
2020-04-26 18:03:50 +0200

    LoginScreen test

--- __tests__/LoginScreen-test.js
+++ __tests__/LoginScreen-test.js
@@ -1,7 +1,11 @@
 import 'react-native';
+import LoginScreen from '../screens/LoginScreen';
 
 const login = jest.fn(x, y);
 
+var log = new LoginScreen();
+
+// Static tests
 test('Correct Mail', () => {
   expect(login("test@test.com", "Asdqwe123")).toBe(true);
   expect(login("", "")).toBe(true);
@@ -16,3 +20,31 @@ test('Empty Test', () => {
 test('Wrong Test', () => {
   expect(login("test@test.com", "Asdqwe123")).toBe(false);
 });
+
+// Dinamic tests
+test('Correct syntax Test', () => {
+  expect(log.state.email).toContain("@");
+  expect(log.state.email).toContain(".");
+});
+
+test('Incorrect syntax Test', () => {
+  function syntaxScan() {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(log.state.email)){
+      
+     } else {
+      throw new ErrorObject('Syntax error');
+     }
+  }
+
+  expect(syntaxScan()).toThrowError(ErrorObject);
+});
+
+test('Throw email error', () => {
+  const loginScreen = new LoginScreen();
+  expect(() => loginScreen.signInWithEmail()).toThrow();
+})
+
+test('Throw facebook error', () => {
+  const loginScreen = new LoginScreen();
+  expect(() => loginScreen.signInWithFacebook()).toThrow();
+})

689663cf6f72c0587dbf5d048f199b0cc61c48cd
LexFTW <lexmengual@gmail.com>
2020-05-01 17:55:30 +0200

    userscreen done

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,14 +1,21 @@
 import * as React from 'react';
+
 import { ScrollView, Text, TextInput, SafeAreaView, Alert, View, Button  } from 'react-native';
-import  base  from '../constants/styles/Styles';
 import { IconButton, Colors, Divider, Avatar } from 'react-native-paper';
 import RNPickerSelect from 'react-native-picker-select';
 import NavigationTop from './../components/NavigationTop';
+
+import  base  from '../constants/styles/Styles';
+
 import Resources from './../config/resources/resources';
+
 import firebase from '../config/firebase';
 import 'firebase/firestore';
 
+const firestore = firebase.firestore();
+
 export default class UserScreen extends React.Component {
+
   constructor(props){
     super(props);
     this.state = ({
@@ -29,6 +36,35 @@ export default class UserScreen extends React.Component {
     });
   }
 
+  async componentDidMount(){
+    const user = firebase.auth().currentUser;
+
+    await firestore
+    .collection('userParametersMedication')
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        Alert.alert('No such document!');
+      } else {
+        this.setState({interval_max: doc.data().GlucoseIntervalMax});
+        this.setState({interval_min: doc.data().GlucoseIntervalMin});
+        this.setState({hc_brekfast: doc.data().HCBaseBrekfast});
+        this.setState({hc_food: doc.data().HCBaseFood});
+        this.setState({hc_snack: doc.data().HCBaseSnack});
+        this.setState({hc_dinner: doc.data().HCBaseDinner});
+        this.setState({insulin_brekfast: doc.data().InsulinBaseBrekfast});
+        this.setState({insulin_food: doc.data().InsulinBaseFood});
+        this.setState({insulin_snack: doc.data().InsulinBaseSnack});
+        this.setState({insulin_dinner: doc.data().InsulinBaseDinner});
+        this.setState({range_over_hc: doc.data().RangeHCHypers});
+        this.setState({range_under_hc: doc.data().RangeHCHipos});
+        this.setState({range_over_insulin: doc.data().RangeInsulinHypers});
+        this.setState({range_under_insulin: doc.data().RangeInsulinHipos});
+      }
+    })
+  }
+
   onSignOut(){
     firebase
       .auth()
@@ -41,17 +77,11 @@ export default class UserScreen extends React.Component {
       });
   }
 
-  async onChangeState(name, value){
-    await this.setState({[name] : value})
-  }
-
-  saveChangesInFirestore(){
+  async saveChangesInFirestore(){
     const user = firebase.auth().currentUser;
-    const doc = firebase.firestore().collection('userParametersMedication').doc(user.uid);
+    const doc = firestore.collection('userParametersMedication').doc(user.uid);
 
-    Alert.alert(this.state.interval_max)
-
-    doc.set({
+    await doc.set({
       GlucoseIntervalMax: this.state.interval_max,
       GlucoseIntervalMin: this.state.interval_min,
       HCBaseBrekfast: this.state.hc_brekfast,
@@ -94,28 +124,70 @@ export default class UserScreen extends React.Component {
         </View>
         <View style={{alignItems: "center", flex: 1, justifyContent: "center", marginVertical: 15}}>
           <View style={{borderColor: '#0069d9', borderWidth: 2, marginBottom: 10, backgroundColor: '#adccea'}}>
-            <UserSettings text={Resources.MEDICATION_GLUCOSE_INTERVAL_MAX} value={this.state.interval_min} label={'interval_min'}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_GLUCOSE_INTERVAL_MIN} number={"1"} value={this.state.interval_max} />
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_GLUCOSE_INTERVAL_MAX}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.interval_max} placeholderTextColor={"#444"} onChangeText={interval_max => this.setState({ interval_max })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_GLUCOSE_INTERVAL_MIN}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.interval_min} placeholderTextColor={"#444"} onChangeText={interval_min => this.setState({ interval_min })} />
+            </View>
           </View>
           <View style={{borderColor: '#0069d9', borderWidth: 2, marginBottom: 10, backgroundColor: '#adccea'}}>
-            <UserSettings lines={2} text={Resources.MEDICATION_HC_BASE_BREKFAST} number={"1"} value={this.state.hc_brekfast}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_HC_BASE_FOOD} number={"1"} value={this.state.hc_food}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_HC_BASE_SNACK} number={"1"} value={this.state.hc_snack}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_HC_BASE_DINNER} number={"1"} value={this.state.hc_dinner}/>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_HC_BASE_BREKFAST}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.hc_brekfast} placeholderTextColor={"#444"} onChangeText={hc_brekfast => this.setState({ hc_brekfast })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_HC_BASE_FOOD}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.hc_food} placeholderTextColor={"#444"} onChangeText={hc_food => this.setState({ hc_food })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_HC_BASE_SNACK}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.hc_snack} placeholderTextColor={"#444"} onChangeText={hc_snack => this.setState({ hc_snack })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_HC_BASE_DINNER}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.hc_dinner} placeholderTextColor={"#444"} onChangeText={hc_dinner => this.setState({ hc_dinner })} />
+            </View>
           </View>
           <View style={{borderColor: '#0069d9', borderWidth: 2, marginBottom: 10, backgroundColor: '#adccea'}}>
-            <UserSettings lines={2} text={Resources.MEDICATION_INSULIN_BASE_BREKFAST} number={"1"} value={this.state.insulin_brekfast}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_INSULIN_BASE_FOOD} number={"1"} value={this.state.insulin_food}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_INSULIN_BASE_SNACK} number={"1"} value={this.state.insulin_snack}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_INSULIN_BASE_DINNER} number={"1"} value={this.state.insulin_dinner}/>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_INSULIN_BASE_BREKFAST}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.insulin_brekfast} placeholderTextColor={"#444"} onChangeText={insulin_brekfast => this.setState({ insulin_brekfast })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_INSULIN_BASE_FOOD}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.insulin_food} placeholderTextColor={"#444"} onChangeText={insulin_food => this.setState({ insulin_food })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_INSULIN_BASE_SNACK}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.insulin_snack} placeholderTextColor={"#444"} onChangeText={insulin_snack => this.setState({ insulin_snack })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_INSULIN_BASE_DINNER}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.insulin_dinner} placeholderTextColor={"#444"} onChangeText={insulin_dinner => this.setState({ insulin_dinner })} />
+            </View>
           </View>
           <View style={{borderColor: '#0069d9', borderWidth: 2, marginBottom: 10, backgroundColor: '#adccea'}}>
-            <UserSettings lines={2} text={Resources.MEDICATION_RANGE_HC_UNDER_INTERVAL_MIN} number={"1"} value={this.state.range_under_hc}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_RANGE_HC_OVER_INTERVAL_MAX} number={"1"} value={this.state.range_over_hc}/>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_RANGE_HC_UNDER_INTERVAL_MIN}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.range_under_hc} placeholderTextColor={"#444"} onChangeText={range_under_hc => this.setState({ range_under_hc })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_RANGE_HC_OVER_INTERVAL_MAX}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.range_over_hc} placeholderTextColor={"#444"} onChangeText={range_over_hc => this.setState({ range_over_hc })} />
+            </View>
           </View>
           <View style={{borderColor: '#0069d9', borderWidth: 2, marginBottom: 10, backgroundColor: '#adccea'}}>
-            <UserSettings lines={2} text={Resources.MEDICATION_RANGE_INSULIN_UNDER_INTERVAL_MIN} number={"1"} value={this.state.range_under_insulin}/>
-            <UserSettings lines={2} text={Resources.MEDICATION_RANGE_INSULIN_OVER_INTERVAL_MAX} number={"1"} value={this.state.range_over_insulin}/>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_RANGE_INSULIN_UNDER_INTERVAL_MIN}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.range_under_insulin} placeholderTextColor={"#444"} onChangeText={range_under_insulin => this.setState({ range_under_insulin })} />
+            </View>
+            <View style={{paddingHorizontal: 15, width: 350, alignItems: "center", justifyContent: "space-between", paddingVertical: 5, flexDirection: "row"}}>
+              <Text style={{fontSize: 15, textTransform:"uppercase"}}>{Resources.MEDICATION_RANGE_INSULIN_OVER_INTERVAL_MAX}</Text>
+              <TextInput keyboardType={'numeric'} placeholder={'Values'} value={this.state.range_over_insulin} placeholderTextColor={"#444"} onChangeText={range_over_insulin => this.setState({ range_over_insulin })} />
+            </View>
           </View>
           <Button
             title="Insertar datos"

b21c1ba082ee721d14fa59d094fa44df778fc5ac
LexFTW <lexmengual@gmail.com>
2020-05-01 02:18:57 +0200

    userscreen

--- config/resources/ca_ES.js
+++ config/resources/ca_ES.js
@@ -1,16 +1,36 @@
 const Resources = {
+  DEMO_HOME: 'Home Screen',
+
   LOGIN_EMAIL: 'Correu Electrònic',
   LOGIN_PASSWORD: 'Contrasenya',
   LOGIN_SIGNIN: 'Iniciar Sessió',
   LOGIN_SIGNIN_GOOGLE: 'Iniciar Sessió amb Google',
   LOGIN_SIGNIN_FACEBOOK: 'Iniciar Sessió amb Facebook',
   LOGIN_REGISTER: "Registra't",
+
   READER_TITLE: 'Lector de glucosa',
   READER_TEXT_BUTTON: 'Comença a llegir',
   READER_HEADER: 'Lector',
-  PROFILE_HEADER: 'Perfil',
+
   PROFILE_SETTINGS_PROFILE: 'Perfil',
   PROFILE_SETTINGS_MEDICATION: 'Tractament',
+  PROFILE_DIABETIS_TYPE: 'Diabetes Tipus',
+
+  MEDICATION_GLUCOSE_INTERVAL_MAX: 'Glucosa màxima',
+  MEDICATION_GLUCOSE_INTERVAL_MIN: 'Glucosa mínima',
+  MEDICATION_HC_BASE_BREKFAST: 'Hidrats de Carboni \n(Esmorzar)' ,
+  MEDICATION_HC_BASE_FOOD: 'Hidrats de Carboni \n(Menjar)' ,
+  MEDICATION_HC_BASE_SNACK: 'Hidrats de Carboni \n(Berenar)' ,
+  MEDICATION_HC_BASE_DINNER: 'Hidrats de Carboni \n(Sopar)' ,
+  MEDICATION_INSULIN_BASE_BREKFAST: "Unitats d'insulina \n(Esmorzar)",
+  MEDICATION_INSULIN_BASE_FOOD: "Unitats d'insulina \n(Menjar)",
+  MEDICATION_INSULIN_BASE_SNACK: "Unitats d'insulina \n(Berenar)",
+  MEDICATION_INSULIN_BASE_DINNER: "Unitats d'insulina \n(Sopar)",
+  MEDICATION_RANGE_HC_UNDER_INTERVAL_MIN: "Resta d'unitats d'insulina \nper rang inferior \nd'hidrats de carboni",
+  MEDICATION_RANGE_HC_OVER_INTERVAL_MAX: "Suma d'unitats d'insulina \nper rang superior \nd'hidrats de carboni",
+  MEDICATION_RANGE_INSULIN_UNDER_INTERVAL_MIN: "Resta d'unitats d'insulina \nper rang inferior \nde la glucosa màxima",
+  MEDICATION_RANGE_INSULIN_OVER_INTERVAL_MAX: "Suma d'unitats d'insulina \nper rang superior \nde la glucosa màxima",
+  MEDICATION_DIABETES_TYPE: 'Tipus de Diabetes'
 }
 
 export default Resources;
--- config/resources/en_US.js
+++ config/resources/en_US.js
@@ -1,16 +1,36 @@
 const Resources = {
   DEMO_HOME: 'Home Screen',
+
   LOGIN_EMAIL: 'Email',
   LOGIN_PASSWORD: 'Password',
   LOGIN_SIGNIN: 'Sign In',
   LOGIN_SIGNIN_GOOGLE: 'Sign In with Google',
   LOGIN_SIGNIN_FACEBOOK: 'Sign In with Facebook',
   LOGIN_REGISTER: 'Sign Up',
+
   READER_TITLE: 'Glucose Reader',
   READER_TEXT_BUTTON: 'Start Reading',
   READER_HEADER: 'Reader',
+
   PROFILE_SETTINGS_PROFILE: 'Profile',
   PROFILE_SETTINGS_MEDICATION: 'Medication',
+  PROFILE_DIABETIS_TYPE: 'Diabetis Type',
+
+  MEDICATION_GLUCOSE_INTERVAL_MAX: 'Maximum Glucose',
+  MEDICATION_GLUCOSE_INTERVAL_MIN: 'Minimum Glucose',
+  MEDICATION_HC_BASE_BREKFAST: 'Carbohydrates \n(Brekfast)' ,
+  MEDICATION_HC_BASE_FOOD: 'Carbohydrates \n(Food)' ,
+  MEDICATION_HC_BASE_SNACK: 'Carbohydrates \n(Snack)' ,
+  MEDICATION_HC_BASE_DINNER: 'Carbohydrates \n(Dinner)' ,
+  MEDICATION_INSULIN_BASE_BREKFAST: 'Insulin Units \n(Brekfast)',
+  MEDICATION_INSULIN_BASE_FOOD: 'Insulin Units \n(Food)',
+  MEDICATION_INSULIN_BASE_SNACK: 'Insulin Units \n(Snack)',
+  MEDICATION_INSULIN_BASE_DINNER: 'Insulin Units \n(Dinner)',
+  MEDICATION_RANGE_HC_UNDER_INTERVAL_MIN: 'Insulin range under \ncarbohydrates base',
+  MEDICATION_RANGE_HC_OVER_INTERVAL_MAX: 'Insulin range over \ncarbohydrates base',
+  MEDICATION_RANGE_INSULIN_UNDER_INTERVAL_MIN: 'Insulin units under \nglucose interval',
+  MEDICATION_RANGE_INSULIN_OVER_INTERVAL_MAX: 'Insulin units over \nglucose interval',
+  MEDICATION_DIABETES_TYPE: 'Diabetes Type'
 }
 
 export default Resources;

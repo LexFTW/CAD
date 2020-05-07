27f268b6df493d0b0f65463d0c5d36db83fc1a50
David <xldaviduson@gmail.com>
2020-05-07 20:40:53 +0200

    Start modify ReaderScreen

--- constants/styles/ReaderStyles.js
+++ constants/styles/ReaderStyles.js
@@ -3,29 +3,7 @@ import { StyleSheet, Platform } from 'react-native';
 const styles = StyleSheet.create({
   icon:{
     color: '#2069b2',
-  },
-  textIcon:{
-    color: '#2069b2',
-    fontSize: 20,
-  },
-  scanButton: {
-    fontSize: 10,
-    backgroundColor: '#2069b2',
-    textAlign: 'center',
-    borderRadius: 6,
-    paddingTop: 15,
-    paddingBottom: 15,
-    marginTop: 30,
-    marginLeft: 40,
-    marginRight: 40
-  },
-  scanButtonText: {
-    fontSize: 25,
-    fontWeight:'bold',
-    color: 'white',
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
+  }
 });
 
 export default styles;
--- screens/ReaderScreen.js
+++ screens/ReaderScreen.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
-import { Text, SafeAreaView } from 'react-native';
-import { Button} from 'react-native-paper';
+import { Button, Text, SafeAreaView, Alert, View } from 'react-native';
+import { IconButton, Colors } from 'react-native-paper';
 import TabBarIconFontAwesome from '../components/TabBarIconFontAwesome';
 
 import Resources from './../config/resources/resources';
@@ -13,22 +13,54 @@ import  styles  from '../constants/styles/ReaderStyles';
 
 export default class ReaderScreen extends Component {
 
-  render(){
-    return (
-      <SafeAreaView style={base.container}>
-        <NavigationTop
-          title={Resources.READER_HEADER}
-        />
-        <TabBarIconFontAwesome style={styles.icon} name="wifi" size={200} color="rgb(97,174,172)" />
-        <Text style={styles.textIcon}>{Resources.READER_TITLE}</Text>
-        <Button
+  scannButton() {
+    Alert.alert("Hola");
+  }
+
+  /**<Button
           mode="contained"
           style={base.btnPrimary}
         >
           <Text style={{fontSize: 25}}>{Resources.READER_TEXT_BUTTON}</Text>
         </Button>
+ */
+
+  render(){
+    return (
+      <SafeAreaView style={{
+        flex: 1,
+        backgroundColor: '#fafafa',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <NavigationTop
+          title={Resources.READER_HEADER}
+        />
+
+        <IconButton
+          icon="wifi"
+          color={Colors.blue600}
+          size={150}
+          onPress={() => this.scannButton()}
+        />
+                 
+        <View style={{paddingTop: 20}}>
+          <HistoryComponent text={"BrekfastValue"} value={"Valor 1"}/>
+          <HistoryComponent text={"FoodValue"} value={"Valor 2"}/>
+          <HistoryComponent text={"SnackValue"} value={"Valor 3"}/>
+          <HistoryComponent text={"DinnerValue"} value={"Valor 4"}/>
+        </View>
       </SafeAreaView>
     );
   }
 
+}
+
+function HistoryComponent({text, value}) {
+  return (
+    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text style={{paddingRight: 20}}>{text}</Text>
+        <Text>{value}</Text>
+    </View>
+  );
 }

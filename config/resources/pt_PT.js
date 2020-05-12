6ec941cf65c234dae943226982415b66d54bd9ea
David <xldaviduson@gmail.com>
2020-05-12 16:18:43 +0200

    Added attributs in lenguages (pt_PT)

--- config/resources/pt_PT.js
+++ config/resources/pt_PT.js
@@ -1,5 +1,6 @@
 const Resources = {
   DEMO_HOME: 'Tela Inicio',
+
   LOGIN_USERNAME: 'Nome de usuário',
   LOGIN_EMAIL: 'Correio eletrônico',
   LOGIN_PASSWORD: 'Senha',
@@ -8,11 +9,53 @@ const Resources = {
   LOGIN_SIGNIN_GOOGLE: 'Iniciar Sessão com Google',
   LOGIN_SIGNIN_FACEBOOK: 'Iniciar Sessão com Facebook',
   LOGIN_REGISTER: 'Inscreva-se',
+
   READER_TITLE: 'Leitor de Glicose',
   READER_TEXT_BUTTON: 'Comece a Ler',
   READER_HEADER: 'Leitor',
+
+  PROFILE_HEADER: 'Perfil',
   PROFILE_SETTINGS_PROFILE: 'Perfil',
   PROFILE_SETTINGS_MEDICATION: 'Tratamento',
+  PROFILE_DIABETIS_TYPE: 'Tipo Diabetes',
+
+  MEDICATION_GLUCOSE_INTERVAL_MAX: 'Glicose máxima',
+  MEDICATION_GLUCOSE_INTERVAL_MIN: 'Glicose mínima',
+  MEDICATION_HC_BASE_BREKFAST: 'Hidratos de Carbono \n(Café da manhã)' ,
+  MEDICATION_HC_BASE_FOOD: 'Hidratos de Carbono \n(Alimento)' ,
+  MEDICATION_HC_BASE_SNACK: 'Hidratos de Carbono \n(Lanche)' ,
+  MEDICATION_HC_BASE_DINNER: 'Hidratos de Carbono \n(Jantar)' ,
+  MEDICATION_INSULIN_BASE_BREKFAST: 'Unidades de insulina \n(Café da manhã)',
+  MEDICATION_INSULIN_BASE_FOOD: 'Unidaes de insulina \n(Alimento)',
+  MEDICATION_INSULIN_BASE_SNACK: 'Unidades de insulina \n(Lanche)',
+  MEDICATION_INSULIN_BASE_DINNER: 'Unidades de insulina \n(Jantar)',
+  MEDICATION_RANGE_HC_UNDER_INTERVAL_MIN: 'Subtração de unidades de insulina \npor faixa mais baixa \nde carboidratos',
+  MEDICATION_RANGE_HC_OVER_INTERVAL_MAX: 'Soma de unidades de insulina \npor faixa superior \nde carboidratos',
+  MEDICATION_RANGE_INSULIN_UNDER_INTERVAL_MIN: 'Subtração de unidades de insulina \npor faixa inferior \nde glicose mínima',
+  MEDICATION_RANGE_INSULIN_OVER_INTERVAL_MAX: 'Soma das unidades de insulina \npela faixa superior \nda glicose máxima',
+  MEDICATION_DIABETES_TYPE: 'Tipo de Diabetes',
+  MEDICATION_SAVE_CHANGES: 'Guardar mudanças',
+
+  HISTORYTABVIEW_TITLE_ROUTE_DAILY: 'DIARIAMENTE',
+  HISTORYTABVIEW_TITLE_ROUTE_MONTHLY: 'MENSALMENTE',
+  HISTORYTABVIEW_TITLE_ROUTE_QUARTER: 'TRIMESTRE',
+
+  BREAKFAST: 'Café da manhã',
+  FOOD: 'Alimento',
+  SNACK: 'Lanche',
+  DINNER: 'Jantar',
+
+  MAIL_SUBJECT: 'Relatório',
+  MAIL_BODY_DATE_1: 'Envio o seguinte email com o relatório do dia ',
+  MAIL_BODY_DATE_2: ' que eu registrei:\n',
+  MAIL_BODY_EAG: '\n\tEag: ',
+  MAIL_BODY_HBA1C: '\n\tHba1c: ',
+  MAIL_BODY_HYPER: '\n\tHiperglicemia: ',
+  MAIL_BODY_HYPO: '\n\tHipoglicemia: ',
+  MAIL_BODY_END: '\nEste email foi endossado pela equipe do DCA.',
+
+  ERROR_REGISTER_EMPTY: 'Existem campos vazios, preencha-os todos.',
+  ERROR_REGISTER_PASS: 'Certifique-se de confirmar a senha corretamente.'
 }
 
 export default Resources

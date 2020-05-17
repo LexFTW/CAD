c7579a7e6cd5d48c44ffd43e528e3d25a83cdb9b
LexFTW <lexmengual@gmail.com>
2020-05-17 18:23:16 +0200

    functions mock

--- functions/ReaderFunctions.js
+++ functions/ReaderFunctions.js
@@ -4,6 +4,9 @@ export default {
   connectionToNFCDevice(){
     return true;
   },
+  failedConnectionToNFCDevice(){
+    throw 'La conexión al dispositivo a fallado, revisa que este todo bien';
+  },
   getValueFromNFCDevice(){
     value = Math.floor(Math.random() * (300 - 65)) + 65;
 
@@ -24,6 +27,13 @@ export default {
     throw 'El valor no puede estar vacío.';
   },
   saveInDatabase(value, meal, carbohydrats){
-    return true;
+    if(value != null || value != undefined || meal != null || meal != undefined || carbohydrats != null || carbohydrats != undefined){
+      return true;
+    }
+
+    throw 'No pueden haber valores sin definir, compruebe que ha seguido los pasos correctamente.';
+  },
+  failedToSaveInDatabaseTimeOut(value, meal, carbohydrats){
+    throw 'El tiempo de espera se ha agotado, pruebe otra vez y revise su conexión a Internet';
   }
 }
